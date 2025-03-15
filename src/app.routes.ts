9e741cd2e75de3afdaac24c6438ec329a47abc77
Chylton M.R <chylton.rezende@hotmail.com>
2025-03-15 20:22:39 -0300

    Criado componente 'estoque' e add lib que simula backend json-server

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -4,6 +4,7 @@ import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
+import { EstoqueComponent } from './app/pages/cmr/estoque/estoque.component';
 
 export const appRoutes: Routes = [
     {
@@ -13,7 +14,8 @@ export const appRoutes: Routes = [
             { path: '', component: Dashboard },
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
+            { path: 'chylton', component: EstoqueComponent },
         ]
     },
     { path: 'landing', component: Landing },
