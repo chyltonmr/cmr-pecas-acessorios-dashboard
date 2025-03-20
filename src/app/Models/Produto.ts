528ee97a3d6e20cbfd65645132e1829acb777e9a
Chylton M.R <chylton.rezende@hotmail.com>
2025-03-19 22:28:39 -0300

    UPDATE
    UPDATE

--- src/app/Models/Produto.ts
+++ src/app/Models/Produto.ts
@@ -3,7 +3,7 @@ import { Custo } from "./Custo";
 import { Marca } from "./Marca";
 import { Preco } from "./Preco";
 
-export class Produto {
+export interface Produto {
     id?: string;
     code?: string;
     nome?: string;
@@ -13,38 +13,12 @@ export class Produto {
     categoria?: Categoria;
     imagem?: string;
     avaliacao?: number;
-    marca?: Marca;
-    custos?: Custo[];
-    preco?: Preco[];
-
-    constructor(
-        id?: string,
-        code?: string,
-        nome?: string,
-        descricao?: string,
-        quantidade?: number,
-        statusEstoque?: string,
-        categoria?: Categoria,
-        imagem?: string,
-        avaliacao?: number,
-        marca?: Marca,
-        custos?: Custo[],
-        precos?: Preco[]
-    ) {
-        this.id = id;
-        this.code = code;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.quantidade = quantidade;
-        this.statusEstoque = statusEstoque;
-        this.categoria = categoria;
-        this.imagem = imagem;
-        this.avaliacao = avaliacao;
-        this.marca = marca;
-        this.custos = custos;
-        this.preco = precos;
-    }
+    marca?: string;
+    custos?: string;
+    liquidoPf?: Number;
+    liquidoPj?: Number;
+    margemPf?: Number;
+    margemPj?: Number;
+    precoPf?: Number;
+    precoPj?: Number;
 }
-
-
-
